f41afa79b953c3b401f595705c88425454fe400c
Herbert Holder <herbertholder@Herberts-Air.localdomain>
2022-10-05 15:23:26 -0500

    10/5/22 exercises

---
+++ js/external.js
@@ -0,0 +1,34 @@
+console.log("hello from external");
+
+alert("Welcome to my Website!.");
+let response = prompt("What is your favorite color?");
+
+alert(response + " is my favorite color too!");
+
+
+// Exercise 3 part 1 and 2
+let movieDays = prompt("how many days would you like to rent this movie?");
+let price = 3;
+let total = price * movieDays;
+alert("This is the price of the movie for " + movieDays + " days: " + "$"+total);
+
+
+
+
+
+
+let companyPay = prompt("what is the pay from the company?");
+let companyHours = prompt("what are the hours worked for the company?")
+let result = companyHours * companyPay;
+alert("You worked: " + companyHours + " hour(s) we will pay you: $" + result );
+
+
+
+
+
+// let Amazon = prompt("what is the pay from Amazon?");
+// let facebook = prompt("what is the pay from facebook?");
+
+
+
+
---
+++ js/intro-js.js
@@ -0,0 +1,10 @@
+// "Use strict"
+let greeting = "hello from the external file"
+console.log(greeting);
+let user = prompt('what is your name?');
+
+let isFull = confirm('is the class full')
+
+alert('hello ' + user + " welcome to my website");
+
+myAge = 25;
