01411ed5151b76d937f8f19f51c52cf7a2b75a43
Herbert Holder <herbertholder@Herberts-Air.localdomain>
2022-10-11 10:41:55 -0500

    refactored some code

---
+++ js/warmUp.js
@@ -0,0 +1,15 @@
+// use strict
+(function(){
+
+    function timesFive(number){
+        if (typeof number !== number){
+            console.log("This is not a number");
+            return 0;
+        }else {
+            return number *5;
+        }
+    }// end of function
+
+
+})();
+
