775e9e776301b104ec899c72acbb1963a65ebe43
Herb <herbrholder@gmail.com>
2022-10-25 19:33:07 -0500

    Welcome html

--- js/warmUp.js
+++ js/warmUp.js
@@ -1,15 +1,34 @@
 // use strict
-(function(){
-
-    function timesFive(number){
-        if (typeof number !== number){
-            console.log("This is not a number");
-            return 0;
-        }else {
-            return number *5;
-        }
-    }// end of function
+// (function(){
+function isNum(x){
+    return !isNaN(parseFloat(`${x}`));
+}
 
 
-})();
+    function average(arr) {
+        let i = 0;
+        let sum=0;
+        // if (arr instanceof Array) {
+            while (i < arr.length) {
+               if(isNum(arr[i])){
+                   sum += parseFloat(arr[i]);
+               }else{
+                   return false;
+               }
+                i++;
+            }
+        return sum / arr.length;
+
+
+        // }
+
+        // return false;
+    }// func
+
+
+    console.log(average([1, 2, 3, 4, 5]));
+
+
+
+// })();
 
