547ea63ca1625664ac575b1397e074be780502f7
Herb <herbrholder@gmail.com>
2022-11-30 20:32:21 -0600

    added jquery dom

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,40 +1,40 @@
 "use strict";
 (function(){
     $(function(){
-        // var h1Contents = $('#h1-id').html()
-        // alert(h1Contents);
-        // alert(typeof(h1Contents));
+        var h1Element = $('#h1-id').html()
+        alert(h1Element);
+        alert("typeof h1 content: " + typeof(h1Element));
 
-        // var h1Contents = $('#p-id').html()
-        // alert(h1Contents);
+        h1Element = $('#p-id').html()
+        alert(h1Element);
 
         //         Use the same id on 2 elements. How does this change the jQuery selection?
         // only alerted the first child with same id
-        // var h1Contents = $('#li1-id').html()
-        // alert(h1Contents);
+        h1Element = $('#li1-id').html()
+        alert(h1Element);
 
-        // $('.codeup').css('border', 'solid 1px red');
+        $('.codeup').css('border', 'solid 1px red');
         //     Give another element an id of codeup. Does this element get a border now?
         // does nothing because we're selecting the codeup class not the id
 
-        // $('li').css('font-size', '20px');
-        // $('li').css('background-color', 'yellow');
-        // $('h1').css('background-color', 'yellow');
-        // $('p').css('background-color', 'yellow');
+        $('li').css('font-size', '20px');
+        $('li').css('background-color', 'yellow');
+        $('h1').css('background-color', 'yellow');
+        $('p').css('background-color', 'yellow');
 
-        // var h1Contents = $('h1').html()
-        // alert(h1Contents);
+        h1Element = $('h1').html()
+        alert(h1Element);
 
         // another to select the three selectors h1, p, and li
-        // $('h1, p, li').css('background-color', 'yellow');
+        $('h1, p, li').css('background-color', 'yellow');
         
-        // $('h1').click(function(){
-        //     $('h1').css('background-color', 'green');
-        // });
+        $('h1').click(function(){
+            $('h1').css('background-color', 'green');
+        });
         
-        // $('p').dblclick(function(){
-        //     $('p').css('font-size', '18px');
-        // });
+        $('p').dblclick(function(){
+            $('p').css('font-size', '18px');
+        });
 
         $('li').hover(
             function() {
