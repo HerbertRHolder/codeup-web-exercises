98cdf5f2e94aa55a0361f1659e1af6bd5451e84e
Herb <herbrholder@gmail.com>
2022-11-30 20:54:45 -0600

    ajax blog complete

---
+++ js/ajax-blog.js
@@ -0,0 +1,29 @@
+
+    $(document).ready(function(){
+        
+        var html = "";
+
+        $.get('data/blog.json').done(function(data) {
+
+            console.log(data);
+
+            for (var i = 0; i <= data.length - 1; i++) {
+
+                html += "<div>";
+
+                html += "<h1>" + data[i].title + "</h1>";
+
+                html += "<p>" + data[i].content + "</p>";
+
+                html += "<p>" + data[i].categories.join(', ') + "</p>";
+
+                html += "<p>" + data[i].date + "</p>";
+
+                html += "</div>";
+
+            }
+            $("#posts").html(html);
+
+        })
+    })
+
