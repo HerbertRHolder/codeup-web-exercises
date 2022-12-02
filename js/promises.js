4844b77379e7d4b1557f1fefa9a7714a4bebc326
Herb <herbrholder@gmail.com>
2022-12-02 16:20:28 -0600

    Added the async function

---
+++ js/promises.js
@@ -0,0 +1,15 @@
+
+
+// function github(username, token) {
+//         const appid = {gitToken}
+
+//         fetch('GET /users/{username}/events/public{?per_page,page}', 
+        
+//       ).then(git => {
+//         console.log("git: ",git);
+//       })
+
+// }
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
